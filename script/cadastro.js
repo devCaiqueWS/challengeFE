55064ad4b54321a61ca697a16693d3954526b265
MaiteSavicius <maitesaviciusmenezes@yahoo.com>
2023-05-17 21:59:31 -0300

    Adicionando atributo HTML

--- script/cadastro.js
+++ script/cadastro.js
@@ -2,8 +2,8 @@ function validarEmail() {
     var emailInput = document.getElementById("email");
     var span = document.getElementById("spanEmail");
     var email = emailInput.value;
-    var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-;
+    var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    
     if(regex.test(email)) 
     {
     } 
@@ -17,26 +17,3 @@ function validarEmail() {
         span.style.marginBottom = '4px';
     }
 }
-
-function validarNome(){
-    var nomeInput = document.getElementById("nome");
-    var nome = nomeInput.value.length;
-    var span = document.getElementById("spanNome");
-
-
-    if(nome <= 3)
-    {
-        nomeInput.style.border = '2px solid #e63636'; 
-        span.style.display = 'block';
-        span.style.fontSize = '12px';
-        span.style.color = '#e63636';
-        span.style.textAlign = 'center';
-        span.style.marginBottom = '4px';
-    } 
-    else {
-        console.log('Nome correto')
-    }
-}
-
-//corrigir validar nome
-
